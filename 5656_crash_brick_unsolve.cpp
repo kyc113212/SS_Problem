ab88a32bd6936eee29f536076a5d23e2db373a7e
kyc113212 <35021495+kyc113212@users.noreply.github.com>
2019-08-26 01:23:15 +0900

    Update 5656_crash_brick_unsolve.cpp

--- 5656_crash_brick_unsolve.cpp
+++ 5656_crash_brick_unsolve.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 int N, W, H;
 int map[15][12];
-int maxV = 0;
+int minV = 9999;
+int dx[] = { -1,1,0, 0 };
+int dy[] = { 0,0,1,-1 };
+
+struct pos {
+	int x, y, depth;
+	pos(int a, int b, int c) : y(a), x(b), depth(c) {}
+};
+
+void copy_map(int origin[][12], int copy[][12]) {
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			copy[i][j] = origin[i][j];
+		}
+	}
+}
+
+bool zero_map(int temp[][12]) {
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			if (temp[i][j] != 0)
+				return false;
+		}
+	}
+	return true;
+}
+
+int check_map(int arr[][12]) {
+	int cnt = 0;
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			if (arr[i][j] != 0) {
+				cnt++;
+			}
+		}
+	}
+
+	return cnt;
+}
 
 void rearrange_map(int temp_map[][12]) {
 	for (int i = 0; i < W; i++) {
@@ -19,17 +58,70 @@ void rearrange_map(int temp_map[][12]) {
 						break;
 					}
 				}
-			}			
+			}
 		}
 	}
 }
 
 void solve(vector<int> &tv) {
 
+	int temp_map[15][12];
+	memset(temp_map, 0, sizeof(temp_map));
+	copy_map(map, temp_map);
 
+	
 	for (int i = 0; i < tv.size(); i++) {
 
+		int start_col = tv[i];
+		queue<pos> q;
+
+		for (int j = 0; j < H; j++) {
+			if (temp_map[j][start_col] != 0) {
+				q.push(pos(j, start_col, temp_map[j][start_col]));
+				break;
+			}
+		}
+
+		while (!q.empty()) {
+			int c_x = q.front().x;
+			int c_y = q.front().y;
+			int c_depth = q.front().depth;
+			q.pop();
+			if (c_depth == 1) {
+				temp_map[c_y][c_x] = 0;
+				continue;
+			}
+			else {
+				temp_map[c_y][c_x] = 0;
+				for (int j = 0; j < 4; j++) {
+					int nx = c_x;
+					int ny = c_y;
+					for (int k = 0; k < c_depth - 1; k++) {
+						nx = nx + dx[j];
+						ny = ny + dy[j];
+						if (nx < 0 || ny < 0 || nx >= W || ny >= H)
+							continue;
+						if (temp_map[ny][nx] != 0) {
+							if (temp_map[ny][nx] == 1) {
+								temp_map[ny][nx] = 0;
+								continue;
+							}
+							else {
+								q.push(pos(ny, nx, temp_map[ny][nx]));
+								temp_map[ny][nx] = 0;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		rearrange_map(temp_map);
+
 	}
+
+	int ans = check_map(temp_map);
+	minV = min(minV, ans);
 }
 
 
@@ -57,7 +149,7 @@ int main() {
 	for (int tc = 1; tc <= T; tc++) {
 		cin >> N >> W >> H;
 		memset(map, 0, sizeof(map));
-		maxV = 0;
+		minV = 9999;
 		vector<int> v;
 
 		for (int i = 0; i < H; i++) {
@@ -67,7 +159,8 @@ int main() {
 		}
 
 		dfs(v, 0);
-		
+
+		cout << "#" << tc << " " << minV << endl;
 
 	}
 
