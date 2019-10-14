351444c7bade03e3ecbc9879045c873aa1f98773
kyc113212 <35021495+kyc113212@users.noreply.github.com>
2019-10-15 00:21:10 +0900

    Update 2477_car_repair_shop_unsolve.cpp

--- 2477_car_repair_shop_unsolve.cpp
+++ 2477_car_repair_shop_unsolve.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <cstring>
 #include <algorithm>
@@ -15,6 +14,7 @@ int finish_depart[1002];
 int finished[1002];
 int using_recept[9];
 int using_repair[9];
+int finish_recept[1002];
 
 struct data1 {
 	int recept, repair, time;
@@ -67,6 +67,7 @@ int main() {
 		memset(finish_depart, 0, sizeof(finish_depart));
 		memset(using_recept, 0, sizeof(using_recept));
 		memset(using_repair, 0, sizeof(using_repair));
+		memset(finish_recept, 0, sizeof(finish_recept));
 		cin >> N >> M >> K >> A >> B;
 		vector<data1> v[1002];
 		vector<waiting_data> recept_space[9];
@@ -96,49 +97,63 @@ int main() {
 
 			//recept는 recept에서 모두 처리
 			for (int i = 1; i <= K; i++) {//i는 사람
-				if (finish_depart[i] == 0 && v[i][0].time == 0) {
-					if (check_recept() == true) {//recept가 full
-						finish_depart[i] = 1; //1013수정
-						wating_recept.push(waiting_data(i, 0));
-					}
-					else {
-						for (int j = 0; j < N; j++) {	//접수창구
-							if (using_recept[j] == 0 && finish_depart[i] == 0 && recept_space[j].empty()) {
-								finish_depart[i] = 1;	//1013수정
-								using_recept[j] = 1;
-								recept_space[j].push_back(waiting_data(i, at[j]));
+				
+				for (int k = 0; k < M; k++) {//i는 수리창구
+					if (using_repair[k] == 1) {
+						//repair_space[i][0].time--;
+						if (repair_space[k][0].time == 0) { //repair 시간 다 끝나면 접수창구 확인하고 finish check
+							v[repair_space[k][0].person_number][0].repair = k + 1;
+							if (v[repair_space[k][0].person_number][0].repair == B && v[repair_space[k][0].person_number][0].recept == A) {
+								ans += repair_space[k][0].person_number;
 							}
-							else if (using_recept[j] == 0 && !recept_space[j].empty()) {
-								finish_depart[i] = 1;
-								using_recept[j] = 1;
-								recept_space[j][0].person_number = i;
-								recept_space[j][0].time = at[j];
+							finished[repair_space[k][0].person_number] = 1;
+							//using_repair[i] = 0;
+							//repair_space[i][0].person_number = 0;
+							//repair_space[i][0].time = 0;
+							if (!wating_repair.empty()) {
+								repair_space[k][0].person_number = wating_repair.front().person_number;
+								repair_space[k][0].time = bt[k];
+								wating_repair.pop();
 							}
+							else {
+								using_repair[k] = 0;
+								repair_space[k][0].person_number = 0;
+								repair_space[k][0].time = 0;
+							}
+							//repair_space[i].pop_back();
 						}
+
 					}
 				}
+				
 				for (int j = 0; j < N; j++) {
 					if (using_recept[j] == 1) {
 						if (recept_space[j][0].time == 0) {
-							v[recept_space[j][0].person_number][0].recept = j + 1;	//recept_space 기억
+							v[recept_space[j][0].person_number][0].recept = j + 1;	//recept_space 기억							
+
 							if (check_repair() == true) {
 								wating_repair.push(waiting_data(recept_space[j][0].person_number, 0));
 							}
 							else {
+
 								for (int k = 0; k < M; k++) {	//수리창구
-									if (using_repair[k] == 0 && repair_space[k].empty()) {
+									if (using_repair[k] == 0 && repair_space[k].empty() && finish_recept[recept_space[j][0].person_number] == 0) {
 										using_repair[k] = 1;
+										finish_recept[recept_space[j][0].person_number] = 1;
 										repair_space[k].push_back(waiting_data(recept_space[j][0].person_number, bt[k]));
 										break;
 										//역에 push back이 두번됨
 									}
-									else if (using_repair[k] == 0 && !repair_space[k].empty()) {
+									else if (using_repair[k] == 0 && !repair_space[k].empty() && finish_recept[recept_space[j][0].person_number] == 0) {
 										using_repair[k] = 1;
+										finish_recept[recept_space[j][0].person_number] = 1;
 										repair_space[k][0].person_number = recept_space[j][0].person_number;
-										repair_space[k][0].time = bt[j];
+										repair_space[k][0].time = bt[k];
 									}
 								}
 							}
+
+
 							if (!wating_recept.empty()) {
 								recept_space[j][0].person_number = wating_recept.front().person_number;
 								recept_space[j][0].time = at[j];
@@ -152,105 +167,38 @@ int main() {
 
 						}
 					}
-
-					for (int k = 0; k < M; k++) {//i는 수리창구
-						if (using_repair[k] == 1) {
-							//repair_space[i][0].time--;
-							if (repair_space[k][0].time == 0) { //repair 시간 다 끝나면 접수창구 확인하고 finish check
-								v[repair_space[k][0].person_number][0].repair = k + 1;
-								if (v[repair_space[k][0].person_number][0].repair == B && v[repair_space[k][0].person_number][0].recept == A) {
-									ans += repair_space[k][0].person_number;
-								}
-								finished[repair_space[k][0].person_number] = 1;
-								//using_repair[i] = 0;
-								//repair_space[i][0].person_number = 0;
-								//repair_space[i][0].time = 0;
-								if (!wating_repair.empty()) {
-									repair_space[k][0].person_number = wating_repair.front().person_number;
-									repair_space[k][0].time = bt[k];
-									wating_repair.pop();
-								}
-								else {
-									using_repair[k] = 0;
-									repair_space[k][0].person_number = 0;
-									repair_space[k][0].time = 0;
-								}
-								//repair_space[i].pop_back();
+				
+				
+				if (finish_depart[i] == 0 && v[i][0].time == 0) {
+					if (check_recept() == true) {//recept가 full
+						finish_depart[i] = 1; //1013수정
+						wating_recept.push(waiting_data(i, 0));
+					}
+					else {
+						for (int j = 0; j < N; j++) {	//접수창구
+							if (using_recept[j] == 0 && finish_depart[i] == 0 && recept_space[j].empty()) {
+								finish_depart[i] = 1;	//1013수정
+								using_recept[j] = 1;
+								recept_space[j].push_back(waiting_data(i, at[j]));
 							}
-
+							else if (using_recept[j] == 0 && !recept_space[j].empty() && finish_depart[i] == 0) {
+								finish_depart[i] = 1;
+								using_recept[j] = 1;
+								recept_space[j][0].person_number = i;
+								recept_space[j][0].time = at[j];
+							}
 						}
 					}
+				}
+
+				
+
+					
 
 
 
 				}
 			}
-
-			/*for (int i = 0; i < N; i++) {//i는 접수창구
-				if (using_recept[i] == 1) {
-					//recept_space[i][0].time--;
-					if (recept_space[i][0].time == 0) {//recept 끝나면 repair로 감
-						v[recept_space[i][0].person_number][0].recept = i + 1;	//recept_space 기억
-						if (check_repair() == true) {
-							wating_repair.push(waiting_data(recept_space[i][0].person_number, 0));
-						}
-						else {
-							for (int j = 0; j < M; j++) {	//수리창구
-								if (using_repair[j] == 0 && repair_space[j].empty()) {
-									using_repair[j] = 1;
-									repair_space[j].push_back(waiting_data(recept_space[i][0].person_number, bt[j]));
-									break;
-									//역에 push back이 두번됨
-								}
-								else if (using_repair[j] == 0 && !repair_space[j].empty()) {
-									using_repair[j] = 1;
-									repair_space[j][0].person_number = recept_space[i][0].person_number;
-									repair_space[j][0].time = bt[j];
-								}
-							}
-						}
-						if (!wating_recept.empty()) {
-							recept_space[i][0].person_number = wating_recept.front().person_number;
-							recept_space[i][0].time = at[i];
-							wating_recept.pop();
-						}
-						else {
-							using_recept[i] = 0;
-							recept_space[i][0].person_number = 0;
-							recept_space[i][0].time = 0;
-						}
-						//recept_space[i].pop_back();
-					}
-				}
-			}
-
-			for (int i = 0; i < M; i++) {//i는 수리창구
-				if (using_repair[i] == 1) {
-					//repair_space[i][0].time--;
-					if (repair_space[i][0].time == 0) { //repair 시간 다 끝나면 접수창구 확인하고 finish check
-						v[repair_space[i][0].person_number][0].repair = i + 1;
-						if (v[repair_space[i][0].person_number][0].repair == B && v[repair_space[i][0].person_number][0].recept == A) {
-							ans += repair_space[i][0].person_number;
-						}
-						finished[repair_space[i][0].person_number] = 1;
-						//using_repair[i] = 0;
-						//repair_space[i][0].person_number = 0;
-						//repair_space[i][0].time = 0;
-						if (!wating_repair.empty()) {
-							repair_space[i][0].person_number = wating_repair.front().person_number;
-							repair_space[i][0].time = bt[i];
-							wating_repair.pop();
-						}
-						else {
-							using_repair[i] = 0;
-							repair_space[i][0].person_number = 0;
-							repair_space[i][0].time = 0;
-						}
-						//repair_space[i].pop_back();
-					}
-
-				}
-			}*/
 
 			c_time++;
 
